85ff29fecd6b60c1c43196965429af64b48f2ac3
Santiago Dubon Sanchez <72222132+sdubonsa@users.noreply.github.com>
2026-05-10 13:25:55 -0400

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hash_map = {}
+
+        for num in nums:
+            if num in hash_map:
+                return True
+            
+            hash_map[num] = 'SEEN'
+        
+        return False
+        
